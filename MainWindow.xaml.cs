e30488d3fe8b3e9e3069690ec02a90ebad44f0fd
Lenodez <66466210+Lenodez@users.noreply.github.com>
2020-10-19 12:50:05 +0300

    Кнопка реализованна в XAML

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,12 +23,6 @@ namespace WPFTrain
         public MainWindow()
         {
             InitializeComponent();
-
-            Button myButton = new Button();
-            myButton.Width = 100;
-            myButton.Height = 30;
-            myButton.Content = "Кнопка";
-            layoutGrid.Children.Add(myButton);
         }
 
         
